bf14c91a382f7f097005a5a78077eef65b9e91fb
KristelWhite <KristelWhite@users.noreply.github.com>
2020-05-03 22:41:24 +0300

    add controllers

---
+++ Codenames/Controllers/GameOverController/GameOverViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameOverViewController.swift
+//  Codenames
+//
+//  Created by Кристина Пастухова on 03.05.2020.
+//  Copyright © 2020 Кристина Пастухова. All rights reserved.
+//
+
+import UIKit
+
+class GameOverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
